joestringer/pcsx2

USB: Add support for Type 2 DenshaCon
Add support for TCPP20009 controllers, datasheets courtesy of Marc Riera. Tested with a One Handle MasCon for Nintendo Switch as the controller device.

Link: https://marcriera.github.io/ddgo-controller-docs/controllers/usb/tcpp20009/

pcsx2/USB/deviceproxy.cpp
@@ -5,6 +5,7 @@
 #include "usb-eyetoy/usb-eyetoy-webcam.h"
 #include "usb-pad/usb-buzz.h"
 #include "usb-pad/usb-gametrak.h"
+#include "usb-pad/usb-mascon.h"
 #include "usb-pad/usb-realplay.h"
 #include "usb-hid/usb-hid.h"
 #include "usb-mic/usb-headset.h"
@@ -84,6 +85,7 @@ void RegisterDevice::Register()
 	inst.Add(DEVTYPE_GUNCON2, new usb_lightgun::GunCon2Device());
 	inst.Add(DEVTYPE_GAMETRAK, new usb_pad::GametrakDevice());
 	inst.Add(DEVTYPE_REALPLAY, new usb_pad::RealPlayDevice());
+	inst.Add(DEVTYPE_MASCON, new usb_pad::MasconDevice());
 }
 
 void RegisterDevice::Unregister()

EmulationCollective/XBSX2

pcsx2/USB/deviceproxy.cpp
@@ -11,6 +11,7 @@
 #include "usb-mic/usb-mic.h"
 #include "usb-msd/usb-msd.h"
 #include "usb-pad/usb-pad.h"
+#include "usb-pad/usb-train.h"
 #include "usb-pad/usb-trance-vibrator.h"
 #include "usb-pad/usb-turntable.h"
 #include "usb-printer/usb-printer.h"
@@ -83,6 +84,7 @@ void RegisterDevice::Register()
 	inst.Add(DEVTYPE_GUNCON2, new usb_lightgun::GunCon2Device());
 	inst.Add(DEVTYPE_GAMETRAK, new usb_pad::GametrakDevice());
 	inst.Add(DEVTYPE_REALPLAY, new usb_pad::RealPlayDevice());
+	inst.Add(DEVTYPE_TRAIN, new usb_pad::TrainDevice());
 }
 
 void RegisterDevice::Unregister()